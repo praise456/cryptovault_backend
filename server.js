60f82b84d576be9790be1c8e58a099be43854f2c
praise456 <35904244+praise456@users.noreply.github.com>
2025-08-06 06:10:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-   ssl: true,
-})
+const uri = mongodb+srv://admin:<db_password>@cluster0.xly04mf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 .then(() => {
   console.log('MongoDB connected Succesfullly');
  })
