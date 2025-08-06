25abe71ca6c06caf1b530bdf2446f14157221d10
praise456 <35904244+praise456@users.noreply.github.com>
2025-08-06 05:32:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
+
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:<db_password>@cluster0.xly04mf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect(process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   ssl: true,
+})
+.then(() => {
+  console.log('MongoDB connected Succesfullly');
+ })
+.catch((error) => {
+  console.error(' MongoDB Connection error;', error);
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
