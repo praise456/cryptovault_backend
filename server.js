e4f417f583a811d23f38e5d1ba2e54f83fb76af6
praise456 <35904244+praise456@users.noreply.github.com>
2025-08-07 17:46:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ router.get('/investments', authMiddleware, async (req, res) => {
 });
 
 const dashboardRoutes = require('./routes/dashboard'); // path depends on where you saved it
-app.use('/api/user', dashboardRoutes);
+
 
 // ---------- Admin helpers ----------
 async function requireAdmin(req, res, next) {
