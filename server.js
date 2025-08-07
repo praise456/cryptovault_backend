daa99d31c9cb334b7b3137ec2f536cf4bab46d4b
praise456 <35904244+praise456@users.noreply.github.com>
2025-08-07 18:30:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -351,6 +351,6 @@ app.use((err, req, res, next) => {
 });
 
 // ---------- Start server ----------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
